ad3183ed8f2ab082f205d4194e99dfe81e5b8268
Vlado <vladislavsantsevich@gmail.com>
2015-10-22 15:43:57 +0300

    change build.js

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,4 +2,4 @@ var gulp    = require('gulp'),
     config  = require('../config'),
     plumber = require('gulp-plumber');
 
-gulp.task('build', ['html', 'css', 'scripts', 'fonts', 'images', 'js', 'resources']);
+gulp.task('build', ['html', 'css', 'scripts', 'fonts', 'images', 'js', 'copy']);
