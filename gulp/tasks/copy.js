fbdc387386b52440111354bfcb5e1bb74605f23a
Vlado <vladislavsantsevich@gmail.com>
2015-10-22 15:43:13 +0300

    add copy.js

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -2,6 +2,6 @@ var gulp    = require('gulp'),
     config  = require('../config'),
     plumber = require('gulp-plumber');
 
-gulp.task('resources', function() {
+gulp.task('copy', function() {
   gulp.src(config.paths.app.resources).pipe(gulp.dest(config.paths.dist.resources));
 });
