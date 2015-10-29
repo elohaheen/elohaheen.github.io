6b309c8714d76116fe3409b26e382204b37f9426
Vlado <vladislavsantsevich@gmail.com>
2015-10-29 10:44:03 +0200

    change html.js

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,19 +1,10 @@
 var gulp     = require('gulp'),
     config   = require('../config'),
     plumber  = require('gulp-plumber'),
-    prettify = require('gulp-html-prettify'),
     jade     = require('gulp-jade');
 
 gulp.task('html', function() {
   gulp.src(config.paths.app.html)
     .pipe(plumber())
     .pipe(jade())
-    .pipe(prettify({
-          brace_style: 'expand',
-          indent_size: 1,
-          indent_char: '\t',
-          indent_inner_html: true,
-          preserve_newlines: true
-        }))
-    .pipe(gulp.dest(config.paths.dist.html))
 });
