3866e06c8a5e48f5b2870e029c7eaaa1ab928327
Vlado <vladislavsantsevich@gmail.com>
2015-11-02 14:16:19 +0200

    change copy.js

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -1,7 +1,7 @@
 var gulp    = require('gulp'),
-    config  = require('../config'),
+    paths   = require('../paths'),
     plumber = require('gulp-plumber');
 
 gulp.task('copy', function() {
-  gulp.src(config.paths.app.copy).pipe(gulp.dest(config.paths.dist.copy));
+  gulp.src('app/copy/*').pipe(gulp.dest(paths.dist));
 });
