3685d5f609f22ee85861638b885dde9f5700a7fd
Vlado <vladislavsantsevich@gmail.com>
2015-11-02 14:15:54 +0200

    change fonts.js

--- gulp/tasks/fonts.js
+++ gulp/tasks/fonts.js
@@ -1,10 +1,10 @@
 var gulp    = require('gulp'),
-    config  = require('../config'),
+    paths   = require('../paths'),
     plumber = require('gulp-plumber'),
     flatten = require('gulp-flatten');
 
 gulp.task('fonts', function() {
-  gulp.src(config.paths.app.fonts)
-  .pipe(flatten({ includeParents: 0}))
-  .pipe(gulp.dest(config.paths.dist.fonts));
+  gulp.src('app/components/**/*.{eot,ttf,woff,woff2,svg}')
+  .pipe(flatten())
+  .pipe(gulp.dest(paths.fonts));
 });
