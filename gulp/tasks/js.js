d4d5ebb0a80295cedc6835133d50acf3d3175b3d
Vlado <vladislavsantsevich@gmail.com>
2015-11-02 14:15:32 +0200

    change js.js

--- gulp/tasks/js.js
+++ gulp/tasks/js.js
@@ -1,7 +1,7 @@
 var gulp    = require('gulp'),
-    config  = require('../config'),
+    paths  = require('../paths'),
     plumber = require('gulp-plumber');
 
 gulp.task('js', function() {
-  gulp.src(config.paths.app.js).pipe(gulp.dest(config.paths.dist.js));
+  gulp.src(paths.nodeModules).pipe(gulp.dest(paths.js));
 });
