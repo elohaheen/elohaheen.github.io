e8d9d712da6d97f688e5036c6d42efa00ffcd542
Vlado <vladislavsantsevich@gmail.com>
2015-11-02 14:16:13 +0200

    change css.js

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,18 +1,19 @@
 var gulp         = require('gulp'),
-    config       = require('../config'),
+    paths        = require('../paths'),
     postcss      = require('gulp-postcss'),
     plumber      = require('gulp-plumber'),
     minmax       = require('postcss-media-minmax'),
     nested       = require('postcss-nested'),
     vars         = require('postcss-simple-vars'),
     url          = require('gulp-modify-css-urls'),
+    clearfix     = require('postcss-clearfix'),
     color        = require('postcss-color-function'),
     combiner     = require('stream-combiner2'),
     pxtorem      = require('postcss-pxtorem'),
-    lost         = require('lost'),
     autoprefixer = require('autoprefixer'),
     typography   = require('postcss-responsive-type'),
     plumber      = require('gulp-plumber'),
+    lost         = require('lost');
     focus        = require('postcss-focus'),
     order        = require('gulp-order'),
     concat       = require('gulp-concat'),
@@ -25,17 +26,18 @@ gulp.task('css', function () {
     minmax,
     pxtorem,
     typography,
+    clearfix,
+    lost,
     vars,
     focus,
     color,
-    lost,
     autoprefixer({
       browsers: ['last 2 version', 'IE 8', 'IE 9', 'IE 10', 'IE 11', 'Opera 12']
     })
   ];
 
   var combined = combiner.obj([
-    gulp.src(config.paths.app.styles),
+    gulp.src(paths.styles),
     order(['reset.css', 'font.css']),
     postcss(processors),
 
@@ -47,20 +49,20 @@ gulp.task('css', function () {
 
             case 'jpg':
             case 'png':
-              return '../' + config.paths.dist.images.split('/').pop() + '/' + name;
+              return '../' + paths.images.split('/').pop() + '/' + name;
             case 'svg':
             case 'eot':
             case 'ttf':
             case 'woff':
             case 'woff2':
-              return '../' + config.paths.dist.fonts.split('/').pop() + '/' + name;
+              return '../' + paths.fonts.split('/').pop() + '/' + name;
 
           }
     }}),
 
     concat('common.css'),
     cssmin(),
-    gulp.dest(config.paths.dist.styles),
+    gulp.dest(paths.css),
   ]);
   combined.on('error', console.error.bind(console));
 });
