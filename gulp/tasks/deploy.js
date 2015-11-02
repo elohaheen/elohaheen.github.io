5d80ef853e845c99310c0ef9f76b15363eb1526d
Vlado <vladislavsantsevich@gmail.com>
2015-11-02 14:15:59 +0200

    change deploy.js

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,11 +1,11 @@
-var config  = require('../config'),
+var paths   = require('../paths'),
     gulp    = require('gulp'),
     plumber = require('gulp-plumber'),
     build   = require('gulp-gh-pages');
 
 gulp.task('deploy', function () {
   console.log('deploying');
-  return gulp.src('././dist/**')
+  return gulp.src(paths.dist)
   .pipe(plumber())
   .pipe(build().on('error', function(){
     console.log('error', arguments);
